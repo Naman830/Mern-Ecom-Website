8e884de110f69a1695193ed5a1c93f0a025d4d49
Naman <namansingla830@gmail.com>
2025-12-02 13:42:36 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+// packages
+import path from "path";
+import express from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+// Utils
+import connectDB from "./config/db.js";
+
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => console.log(`Server Running on Port : ${port}`));
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1 class="bg-black text-9xl text-white text-center">
+    Hello world!
+  </h1>
+  )
+}
+
+export default App
