614222099b9c2679d032dd53b7ea746ffa41413a
Naman <namansingla830@gmail.com>
2025-12-03 13:46:34 +0530

    getCurrentUserProfile created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -83,3 +83,19 @@ export const getAllUsers = asyncHandler(async(req, res) => {
   const users = await User.find({})
   res.json(users)
 })
+
+// Get Current User Profile
+export const getCurrentUserProfile = asyncHandler(async(req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if(user) {
+    res.json({
+      _id: user._id,
+      username: user.username,
+      email: user.email
+    })
+  } else {
+    res.status(404);
+    throw new Error("User not found 😒")
+  }
+})
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,14 +1,23 @@
 import express from "express";
-import {createUser, loginUser, logutCurrentUser, getAllUsers} from "../controllers/userController.js";
+import {
+  createUser,
+  loginUser,
+  logutCurrentUser,
+  getAllUsers,
+  getCurrentUserProfile,
+} from "../controllers/userController.js";
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 
 // Middlewares
 
-
 const router = express.Router();
 
-router.route("/").post(createUser).get(authenticate, authorizeAdmin, getAllUsers);
-router.post('/auth', loginUser);
-router.post('/logout', logutCurrentUser)
+router
+  .route("/")
+  .post(createUser)
+  .get(authenticate, authorizeAdmin, getAllUsers);
+router.post("/auth", loginUser);
+router.post("/logout", logutCurrentUser);
+router.route("/profile").get(authenticate, getCurrentUserProfile);
 
 export default router;
