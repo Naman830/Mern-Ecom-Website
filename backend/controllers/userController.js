3951290ad750d4c637443c07320b544d897e57b6
Naman <namansingla830@gmail.com>
2025-12-03 13:05:21 +0530

    login user created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,7 @@ import asyncHandler from "../middlewares/asyncHandler.js";
 import bcrypt from "bcryptjs";
 import createToken from "../utils/createToken.js";
 
-const createUser = asyncHandler(async (req, res) => {
+export const createUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
 
   //   check that user fill all nesscarry requirements
@@ -15,7 +15,8 @@ const createUser = asyncHandler(async (req, res) => {
 
   //   check user exist
   const userExists = await User.findOne({ email });
-  if (userExists) res.status(400).send("User Already exists");
+  if (userExists) res.status(400).send("User Already exists")  
+    return;
 
   //   bcryption
   const salt = await bcrypt.genSalt(10);
@@ -39,4 +40,26 @@ const createUser = asyncHandler(async (req, res) => {
   }
 });
 
-export default createUser;
+export const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+
+    if (isPasswordValid) {
+      createToken(res, existingUser._id);
+      res.status(201).json({
+        _id: existingUser._id,
+        username: existingUser.username,
+        email: existingUser.email,
+        isAdmin: existingUser.isAdmin,
+      });
+      return;
+    }
+  }
+});
