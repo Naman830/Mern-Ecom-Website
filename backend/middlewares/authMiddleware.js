ec75c83d37ba91b5aeef22da954eb6d7f2f5ab78
Naman <namansingla830@gmail.com>
2025-12-03 13:24:23 +0530

    authentiction for admin

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -2,7 +2,8 @@ import jwt from "jsonwebtoken";
 import User from "../models/userModel";
 import asyncHandler from "./asyncHandler";
 
-const authenticate = asyncHandler(async (req, res, next) => {
+// Check for user
+export const authenticate = asyncHandler(async (req, res, next) => {
   let token;
 
   // Read JWT from the 'jwt' cookie
@@ -22,3 +23,14 @@ const authenticate = asyncHandler(async (req, res, next) => {
     throw new Error("No authorised, no token");
   }
 });
+
+
+// Check for the Admin 
+export const authorizeAdmin = (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+
+        next();
+    } else {
+        res.status(401).send("Not authorised as an Admin")
+    }
+}
