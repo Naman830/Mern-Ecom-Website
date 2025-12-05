5b85c151cf99a6e37290a8d2ed0fabf7434b9784
Naman <namansingla830@gmail.com>
2025-12-05 12:37:34 +0530

    userlist bbasic set up

--- frontend/src/pages/admin/UserList.jsx
+++ frontend/src/pages/admin/UserList.jsx
@@ -4,7 +4,25 @@ import Loader from "../../components/Loader";
 import { toast } from "react-toastify";
 import Message from "../../components/Message";
 
+import {
+  useGetUsersQuery,
+  useDeleteUserMutation,
+  useUpdateUserMutation,
+} from "../../redux/api/usersApiSlice";
+
 const UserList = () => {
+  const { data: users, refetch, isLoading, error } = useGetUsersQuery();
+  const [deleteUser] = useDeleteUserMutation();
+  const [updateUser] = useUpdateUserMutation();
+
+  const [editableUserId, setEditableUserId] = useState(null);
+  const [editableUserName, setEditableUserName] = useState("");
+  const [editableUserEmail, setEditableUserEmail] = useState("");
+
+  useEffect(() => {
+    refetch();
+  }, [refetch]);
+
   return <div>UserList</div>;
 };
 
