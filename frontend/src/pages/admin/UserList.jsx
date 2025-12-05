486de2201a0c1a5568a82ca17340f63a910833df
Naman <namansingla830@gmail.com>
2025-12-05 14:08:18 +0530

    useer list as admin update, delete etc completed

--- frontend/src/pages/admin/UserList.jsx
+++ frontend/src/pages/admin/UserList.jsx
@@ -23,7 +23,148 @@ const UserList = () => {
     refetch();
   }, [refetch]);
 
-  return <div>UserList</div>;
+  // DELETE USER BY ADMIN
+  const deleteHandler = async (id) => {
+    if (window.confirm("Are you Sure?")) {
+      try {
+        await deleteUser(id);
+      } catch (error) {
+        toast.error(error.data.message || error.error);
+      }
+    }
+  };
+  // TOOGLE EDIT BY ADMIN
+  const toggleEdit = (id, username, email) => {
+    setEditableUserId(id);
+    setEditableUserName(username);
+    setEditableUserEmail(email);
+  };
+  // UPDATE USER BY ADMIN
+  const updateHandler = async (id) => {
+    try {
+      await updateUser({
+        userId: id,
+        username: editableUserName,
+        email: editableUserEmail,
+      });
+
+      setEditableUserId(null);
+      refetch();
+    } catch (error) {
+      toast.error(error);
+    }
+  };
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-semibold mb-4">Users</h1>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
+        <div className="flex flex-col md:flex-row">
+          {/* <AdminMenu /> */}
+          <table className="w-full md:w-4/5 mx-auto">
+            <thead>
+              <tr>
+                <th className="px-4 py-2 text-left">ID</th>
+                <th className="px-4 py-2 text-left">NAME</th>
+                <th className="px-4 py-2 text-left">EMAIL</th>
+                <th className="px-4 py-2 text-left">ADMIN</th>
+                <th className="px-4 py-2"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <tr key={user._id}>
+                  <td className="px-4 py-2">{user._id}</td>
+                  <td className="px-4 py-2">
+                    {editableUserId === user._id ? (
+                      <div className="flex items-center">
+                        <input
+                          type="text"
+                          value={editableUserName}
+                          onChange={(e) => setEditableUserName(e.target.value)}
+                          className="w-full p-2 border rounded-lg"
+                        />
+                        <button
+                          onClick={() => updateHandler(user._id)}
+                          className="ml-2 bg-blue-500 text-white py-2 px-4 rounded-lg"
+                        >
+                          <FaCheck />
+                        </button>
+                      </div>
+                    ) : (
+                      <div className="flex items-center">
+                        {user.username}{" "}
+                        <button
+                          onClick={() =>
+                            toggleEdit(user._id, user.username, user.email)
+                          }
+                        >
+                          <FaEdit className="ml-[1rem]" />
+                        </button>
+                      </div>
+                    )}
+                  </td>
+                  <td className="px-4 py-2">
+                    {editableUserId === user._id ? (
+                      <div className="flex items-center">
+                        <input
+                          type="text"
+                          value={editableUserEmail}
+                          onChange={(e) => setEditableUserEmail(e.target.value)}
+                          className="w-full p-2 border rounded-lg"
+                        />
+                        <button
+                          onClick={() => updateHandler(user._id)}
+                          className="ml-2 bg-blue-500 text-white py-2 px-4 rounded-lg"
+                        >
+                          <FaCheck />
+                        </button>
+                      </div>
+                    ) : (
+                      <div className="flex items-center">
+                        <a href={`mailto:${user.email}`}>{user.email}</a>{" "}
+                        <button
+                          onClick={() =>
+                            toggleEdit(user._id, user.name, user.email)
+                          }
+                        >
+                          <FaEdit className="ml-[1rem]" />
+                        </button>
+                      </div>
+                    )}
+                  </td>
+                  <td className="px-4 py-2">
+                    {user.isAdmin ? (
+                      <FaCheck style={{ color: "green" }} />
+                    ) : (
+                      <FaTimes style={{ color: "red" }} />
+                    )}
+                  </td>
+                  <td className="px-4 py-2">
+                    {!user.isAdmin && (
+                      <div className="flex">
+                        <button
+                          onClick={() => deleteHandler(user._id)}
+                          className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                        >
+                          <FaTrash />
+                        </button>
+                      </div>
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default UserList;
--- frontend/src/redux/api/usersApiSlice.js
+++ frontend/src/redux/api/usersApiSlice.js
@@ -34,37 +34,35 @@ export const userApiSlice = apiSlice.injectEndpoints({
       }),
     }),
 
-    getUsers: builder.mutation({
-      url: USERS_URL,
+     getUsers: builder.query({
+      query: () => ({
+        url: USERS_URL,
+      }),
+      providesTags: ["User"],
+      keepUnusedDataFor: 5,
     }),
-    providesTags: ["User"],
-    keepUnusedDataFor: 5,
-  }),
-
-  deleteUser: builder.mutation({
-    query: (userId) => ({
-      url: `${USERS_URL}/${userId}`,
-      method: "DELETE",
+    deleteUser: builder.mutation({
+      query: (userId) => ({
+        url: `${USERS_URL}/${userId}`,
+        method: "DELETE",
+      }),
     }),
-  }),
-
-  getUserDetails: builder.query({
-    query: (id) => ({
-      url: `${USERS_URL}/${id}`,
+    getUserDetails: builder.query({
+      query: (id) => ({
+        url: `${USERS_URL}/${id}`,
+      }),
+      keepUnusedDataFor: 5,
     }),
-    keepUnusedDataFor: 5,
-  }),
-
-  updateUser: builder.mutation({
-    query: (data) => ({
-      url: `${USERS_URL}/${data.userId}`,
-      method: "PUT",
-      body: data,
+    updateUser: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/${data.userId}`,
+        method: "PUT",
+        body: data,
+      }),
+      invalidatesTags: ["User"],
     }),
-    invalidatesTags: ["User"],
   }),
 });
-
 export const {
   useLoginMutation,
   useLogoutMutation,
