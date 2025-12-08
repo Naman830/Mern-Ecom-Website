2bf2f2dbdf7beb50fac2d48b3e5cc255bc609161
Naman <namansingla830@gmail.com>
2025-12-08 13:44:10 +0530

    category form

--- frontend/src/pages/admin/CategoryList.jsx
+++ frontend/src/pages/admin/CategoryList.jsx
@@ -1,11 +1,28 @@
-import { useState } from "react"
-import { toast } from "react-toastify"
-
+import { useState } from "react";
+import { toast } from "react-toastify";
+import {
+  useCreateCategoryMutation,
+  useDeleteCategoryMutation,
+  useFetchCategoriesQuery,
+  useUpdateCategoryMutation,
+} from "../../redux/api/categoryApiSlice";
 
 const CategoryList = () => {
-  return (
-    <div>CategoryList</div>
-  )
-}
+  const { data: categories } = useFetchCategoriesQuery();
+  const [name, setName] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [updateName, setUpdateName] = useState("");
+  const [modelVisible, setModelVisible] = useState(false);
 
-export default CategoryList
+  const [createCategory] = useCreateCategoryMutation();
+  const [updateCategory] = useUpdateCategoryMutation();
+  const [deleteCategory] = useDeleteCategoryMutation();
+ 
+  return(
+    <div>
+      manage cateogries
+    </div>
+  );
+};
+
+export default CategoryList;
